314bf65730fa9e499911e82ecf84e423ee412616
DongHyeonka <ehdgus6435@kumoh.ac.kr>
2025-05-13 17:37:26 +0900

    Redis 발행-구독 기능 구현 프로젝트 초기 설정
    - Spring Boot와 Redis를 사용한 발행-구독 기능 구현
    - Redis 연결 설정 구성
    - 메시지 발행 컨트롤러 구현
    - 메시지 수신 리스너 서비스 구현
    - Gradle 빌드 환경 설정

---
+++ subpub/src/main/java/com/springboot/subpub/SubpubApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.subpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubpubApplication.class, args);
+	}
+
+}
